b7d5a4bfc88fbc2c30428e0a382ec5e65c642723
Chris McMeeking <chris.mcmeeking@deque.com>
2015-02-04 12:43:30 -0500

    Initial Commit

---
+++ TestSegmentedControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestSegmentedControl
+//
+//  Created by Chris McMeeking on 2/4/15.
+//  Copyright (c) 2015 Chris McMeeking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
